fca6d0d1c5a98d7edd6e63e2cce1fe8f87cb0af7
vilppu <tsvilp@gmail.com>
2020-12-30 20:13:46 +0200

    Build on publish (was not redundant)

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 set -ev
 dotnet restore ./Tests
 dotnet test ./Tests/Tests.fsproj -c Release
-dotnet publish ./HttpApi -c Release -r debian.8-x64 -o Published --no-build
+dotnet publish ./HttpApi -c Release -r debian.8-x64 -o Published
 zip -r jottai.agent.zip ./Published
